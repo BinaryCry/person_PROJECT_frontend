ff478fedfc71410e9ca6d5a5a5e63da10da86632
Oleksandr Stepanenko <ostepanenko@intellias.com>
2019-04-10 17:27:01 +0300

    reselect

--- src/Containers/YesNo/index.tsx
+++ src/Containers/YesNo/index.tsx
@@ -3,11 +3,22 @@ import { connect } from "react-redux";
 import { fetchYesNo } from "actions/yesno";
 import { IStore } from "store";
 import { Dispatch } from "redux";
+import { createSelector } from "reselect";
+
+const yesnoSelector = (state: IStore) => state.yesno;
+const yesnoValueSelector = createSelector(
+  yesnoSelector,
+  yesno => yesno.value
+);
+const yesnoImgSelector = createSelector(
+  yesnoSelector,
+  yesno => yesno.img
+);
 
 const mapStateToProps = (state: IStore) => ({
   yesno: {
-    value: state.yesno.value,
-    img: state.yesno.img
+    value: yesnoValueSelector(state),
+    img: yesnoImgSelector(state)
   }
 });
 
